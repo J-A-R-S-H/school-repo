c379245325b593e015cb8aacccb705207794d959
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2024-02-24 16:24:18 -0800

    fixed footer links in the bottom and align right the picture for students

--- single-fwd-student.php
+++ single-fwd-student.php
@@ -27,9 +27,8 @@ get_header();
 
 				<?php
 
-
 				if (has_post_thumbnail()) {
-					the_post_thumbnail('large');
+					the_post_thumbnail('medium', array('class' => 'alignright'));
 				}
 				the_content();
 
